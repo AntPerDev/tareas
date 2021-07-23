9265c6cf9955cf27f2be6aede022c01222a10487
Antonio Pérez <antperdev@gmail.com>
2021-07-23 08:05:06 +0200

    Añadido todo al Html

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -5,7 +5,7 @@ export class TodoList {
 
   constructor(){
 
-    this.todo = [];
+    this.todos = [];
 
 
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles.css';
-
+import { crearTodoHtml} from './js/componentes';
 
 import { Todo,TodoList } from './classes';
 // import { Todo } from './classes/todo.class';
@@ -8,9 +8,14 @@ import { Todo,TodoList } from './classes';
 // index.js y ya es puede importar todo de ahi
 // mirar archivo index.js dentro de ./classes
 
-const todoList = new TodoList();
+export const todoList = new TodoList();
 
-const tarea = new Todo('Aprender JavaScript'); 
+const tarea = new Todo('Aprender JavaScript!!!'); 
+// const tarea2 = new Todo('Comprar unicornio'); 
 
+todoList.nuevoTodo(tarea);
+// todoList.nuevoTodo(tarea2);
+tarea.completado=true;
+console.log(todoList);
 
-// console.log(tarea);
+crearTodoHtml( tarea );
