402e489bb183d9032f393964ce50fc802fb8a548
Antonio Pérez <antperdev@gmail.com>
2021-07-23 11:13:24 +0200

    Eliminar completados

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -5,6 +5,7 @@ import { todoList } from '../index';
 
 const divTodoList = document.querySelector('.todo-list');
 const txtInput = document.querySelector('.new-todo');
+const btnBorrar = document.querySelector('.clear-completed');
 
 
 
@@ -82,3 +83,21 @@ divTodoList.addEventListener('click', (event) => {
 
 
 });
+
+btnBorrar.addEventListener('click', () =>{
+
+
+    todoList.eliminarCompletados();
+
+    for(let i = divTodoList.children.length-1;i>=0;i--){
+        const elemento = divTodoList.children[i];
+
+        console.log(elemento);
+
+        if(elemento.classList.contains('completed')){
+            divTodoList.removeChild(elemento);
+        }
+        
+    }
+
+})
