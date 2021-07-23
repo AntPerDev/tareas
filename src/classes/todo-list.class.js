38d07b4078bf235aa3df363d167c0bb0805dccf7
Antonio Pérez <antperdev@gmail.com>
2021-07-23 08:40:08 +0200

    eliminar un todo

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -3,34 +3,45 @@ export class TodoList {
 
 
 
-  constructor(){
+  constructor() {
 
     this.todos = [];
 
 
   }
 
-  nuevoTodo ( todo ) {
-    this.todos.push( todo );
+  nuevoTodo(todo) {
+    this.todos.push(todo);
 
 
   }
 
 
-  eliminarTodo(id){
+  eliminarTodo(id) {
+    // documentación de filter
+    // https://developer.mozilla.org/es/docs/orphaned/Web/JavaScript/Reference/Global_Objects/Array/filter
 
+
+    this.todos = this.todos.filter(todo => todo.id != id)
+  }
+
+
+  marcarCompletado(id) {
+    for (const todo of this.todos) {
+
+      console.log(id, todo.id);
+
+      if (todo.id == id) {
+        todo.completado = !todo.completado;
+        break;
+      }
+    }
 
   }
 
 
-  marcarCompletado(ie) {
+  eliminarCompletados() {
 
 
-  }
-
-
-  eliminarCompletados (){
-
-    
   }
 }
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,5 +1,5 @@
-import {Todo} from '../classes';
-import {todoList} from '../index';
+import { Todo } from '../classes';
+import { todoList } from '../index';
 
 //Referencias en el HTML
 
@@ -11,9 +11,9 @@ const txtInput = document.querySelector('.new-todo');
 export const crearTodoHtml = (todo) => {
 
     const htmlTodo = `
-        <li class="${ (todo.completado)?'completed':''}" data-id="${todo.id}">
+        <li class="${(todo.completado) ? 'completed' : ''}" data-id="${todo.id}">
             <div class="view">
-                <input class="toggle" type="checkbox" ${ (todo.completado) ? 'checked' : ''}>
+                <input class="toggle" type="checkbox" ${(todo.completado) ? 'checked' : ''}>
                 <label>${todo.tarea}</label>
                 <button class="destroy"></button>
             </div>
@@ -21,37 +21,64 @@ export const crearTodoHtml = (todo) => {
         </li>
     `;
 
-    const div=document.createElement('div');
+    const div = document.createElement('div');
 
     div.innerHTML = htmlTodo;
 
-    divTodoList.append( div.firstElementChild );
+    divTodoList.append(div.firstElementChild);
 
     return div.firstElementChild;
 
 }
 
 
-txtInput.addEventListener('keyup', (event)=> {
+txtInput.addEventListener('keyup', (event) => {
 
 
-    if( event.keyCode === 13 && txtInput.value.length > 0){
+    if (event.keyCode === 13 && txtInput.value.length > 0) {
 
-        
+
         console.log(txtInput.value)
-        
-        const nuevoTodo = new Todo( txtInput.value);
 
-        todoList.nuevoTodo( nuevoTodo );
+        const nuevoTodo = new Todo(txtInput.value);
+
+        todoList.nuevoTodo(nuevoTodo);
 
         crearTodoHtml(nuevoTodo);
 
         txtInput.value = '';
-        
+
     }
 
     // console.log(event);
+
+
+
+});
+
+
+divTodoList.addEventListener('click', (event) => {
+
+
+    const nombreElemento = event.target.localName;
+    const todoElemento = event.target.parentElement.parentElement;
+    const todoId = todoElemento.getAttribute('data-id');
+
+
+
+    if(nombreElemento.includes('input')){
+        todoList.marcarCompletado(todoId);
+        todoElemento.classList.toggle('completed')
+    } else if (nombreElemento.includes('button')){
+
+        todoList.eliminarTodo ( todoId );
+        divTodoList.removeChild ( todoElemento );
+    }
+
+    // console.log(todoList);
     
+
+
 
 
 });
